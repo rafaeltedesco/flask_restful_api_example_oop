3c15ac28ccd7933d96465550f956c3d78a6b93b6
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2021-05-13 00:00:14 -0300

    post method added

--- resources/persons_api/persons.py
+++ resources/persons_api/persons.py
@@ -1,8 +1,32 @@
 from .data import persons
-from flask_restful import Resource
+from flask import jsonify
+from flask_restful import Resource, reqparse, abort
+
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+
+
+class Person:
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
 
 class Persons(Resource):
 
     def get(self):
         return persons
+
+    def post(self):
+        args = parser.parse_args()
+        person_id = len(persons) + 1
+        new_person = Person(person_id, args['name'])
+        persons.append({
+            'id': new_person.id,
+            'name': new_person.name
+        })
+        return {
+            "status": "OK",
+            "message": "New Person created"
+        }
